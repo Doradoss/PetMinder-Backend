e36d6e0f0c6c72b39ede3cc2e15fba0fca5703b3
Daniel Bustamante <u202211574@upc.edu.pe>
2024-09-16 22:01:04 -0500

    Primera versión del proyecto

---
+++ PetMinder/src/main/java/com/upc/petminder/repository/RecordatoriosRepository.java
@@ -0,0 +1,8 @@
+package com.upc.petminder.repository;
+
+import com.upc.petminder.model.Recordatorios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordatoriosRepository extends JpaRepository<Recordatorios, Long> {
+
+}
